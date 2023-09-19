d8ee5c79a8f8ada2e69554d3e261609f41ab9cf5
loki0b <riqc0nt4ct@hotmail.com>
2023-09-19 10:28:18 -0300

    refactoring

--- shell.c
+++ shell.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 
 #define MAX_LENGTH 40
 #define BIN_PATH "/bin/"
 
 int main(void) {
 	char args[MAX_LENGTH+1], *tokens;
+	char arg_bin[] = BIN_PATH;
 	int run = 1; /* flag to determine to exit program */
-	pid_t pid;
 
 	while (run)	{
 		printf("shell>");
 		fflush(stdout);
+		
 		fgets(args, MAX_LENGTH+1, stdin);
 		args[strcspn(args, "\n")] = 0;
-		char arg_bin[] = BIN_PATH;
 		tokens = strtok(args, " ");
 		strcat(arg_bin, tokens);
 		
