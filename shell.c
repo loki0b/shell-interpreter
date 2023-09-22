953aecadef45ec21931d3a402facc3bdd33e47e0
loki0b <riqc0nt4ct@hotmail.com>
2023-09-22 17:02:08 -0300

    first commit shell execution

--- shell.c
+++ shell.c
@@ -1,13 +1,55 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_LENGTH 80
+
+void separate_tokens(char ch[], char *arr_ch[]);
 
 int main(void) {
-	int run = 1; /* flag to determine when to exit program */
+	char args[MAX_LENGTH+1], *token, *tokens[10];
+	int running; /* flag to determine when to exit program */
+	pid_t pid;
 
-	while (run)	{
-		printf("shell>");
+	running = 1;
+	while (running)	{
+		printf("hgl>");
 		fflush(stdout);
-		run = 0;
+
+		fgets(args, MAX_LENGTH, stdin);
+		args[strcspn(args, "\n")] = 0;
+
+		pid = fork();
+		if (pid < 0) {
+			fprintf(stderr, "Fork failed");
+			return -1;
+		} 
+		else if (pid == 0) {
+			printf("Child process init\n");
+			separate_tokens(&*args, tokens);
+			execvp(tokens[0], tokens);
+		}
+		else {
+			wait(NULL);
+			printf("\nChild process finished\n");
+		}
+
+		running = 0;
 	}
 
 	return 0;
+}
+
+void separate_tokens(char *ch, char *arr_ch[]) {
+	char *token;
+	int t_i = 0;
+
+	token = strtok(ch, " ");
+	do {
+		arr_ch[t_i] = token;
+		token = strtok(NULL, " ");
+		t_i++;
+	} while (token != NULL);
 }
