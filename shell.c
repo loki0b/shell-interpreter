8bb1203895c3a5cd6c382b917112562515622cc3
loki0b <riqc0nt4ct@hotmail.com>
2023-09-25 01:03:55 -0300

    improving code and comments

--- shell.c
+++ shell.c
@@ -5,16 +5,15 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#define MAX_LENGTH 80 /* command line max length */
-#define MAX_TOKENS 40 /* tokens max length */
-#define EXIT "exit"
+#define MAX_LENGTH 80 /* Command line max length */
+#define MAX_TOKENS 40 /* Tokens max length */
 
 void separate_tokens(char *ch, char *arr_ch[]);
 
 int main(int argc, char *argv[]) {
-	char args[MAX_LENGTH+1], *tokens[MAX_TOKENS+1], history[MAX_LENGTH+1];
-	int running; /* flag to determine when to exit program */
-	int run_history;
+	char args[MAX_LENGTH+1], history[MAX_LENGTH+1], *tokens[MAX_TOKENS+1];
+	int running; /* Flag to determine when to exit program */
+	int run_history; /* Flag to determine when execute last command typed */
 	pid_t pid;
 
 	run_history = 0;
@@ -23,27 +22,27 @@ int main(int argc, char *argv[]) {
 		printf("hgl>");
 		fflush(stdout);
 
-		if (argc < 2) { // interactive mode
+		if (argc < 2) { // Interactive mode
 			fgets(args, MAX_LENGTH, stdin);
-		} else if (argc == 2) { // batch mode
+		} else if (argc == 2) { // Batch mode
 			FILE *fptr;
 			fptr = fopen(argv[1], "r");
 			fgets(args, MAX_LENGTH, fptr);
 			running = 0;
-			
 		} else {
 			fprintf(stderr, "Invalid arguments");
 			return -1;
 		}
 
-		args[strcspn(args, "\n")] = 0;
+		args[strcspn(args, "\n")] = 0; // Remove the "\n" char to use exec functions
 
-		if (!(strcmp(args, "!!"))) {
-			run_history = 1;
-		} else strcpy(history, args);
-
-		if (!(strcmp(args, EXIT)))
+		if (!(strcmp(args, "exit")))
 			exit(EXIT_SUCCESS);
+
+		if (!(strcmp(args, "!!"))) 
+			run_history = 1;
+		else 
+			strcpy(history, args);
 
 		pid = fork();
 
@@ -68,6 +67,7 @@ int main(int argc, char *argv[]) {
 }
 
 void separate_tokens(char *ch, char *arr_ch[]) {
+	/* Function to separate args of typed command line */
 	char *token;
 	int t_i = 0;
 
@@ -77,5 +77,5 @@ void separate_tokens(char *ch, char *arr_ch[]) {
 		token = strtok(NULL, " ");
 		t_i++;
 	} while (token != NULL);
-	arr_ch[t_i] = NULL;
+	arr_ch[t_i] = NULL; // Indicate the final of args in array
 }
