6f05bcb689f11adc0644de3efba5e3949bab95f8
loki0b <riqc0nt4ct@hotmail.com>
2023-09-25 17:07:04 -0300

    add features to command line; separation with ; and functions to run the programs and separate tokens

--- shell.c
+++ shell.c
@@ -8,7 +8,8 @@
 #define MAX_LENGTH 80 /* Command line max length */
 #define MAX_TOKENS 40 /* Tokens max length */
 
-void separate_tokens(char *ch, char *arr_ch[]);
+void separate_programs(char *ch, char *arr_ch[]);
+int exec_programs(char *arr_prg);
 
 int main(int argc, char *argv[]) {
 	char args[MAX_LENGTH+1], history[MAX_LENGTH+1], *tokens[MAX_TOKENS+1];
@@ -28,6 +29,8 @@ int main(int argc, char *argv[]) {
 			FILE *fptr;
 			fptr = fopen(argv[1], "r");
 			fgets(args, MAX_LENGTH, fptr);
+			fclose(argv[1]);
+			
 			running = 0;
 		} else {
 			fprintf(stderr, "Invalid arguments");
@@ -44,38 +47,55 @@ int main(int argc, char *argv[]) {
 		else 
 			strcpy(history, args);
 
-		pid = fork();
-
-		if (pid < 0) {
-			fprintf(stderr, "Fork failed");
-			return -1;
-		} 
-		else if (pid == 0) {
-			if (run_history) {
-				separate_tokens(history, tokens);
-			} else {
-				separate_tokens(args, tokens);
+		if (run_history) {
+			separate_programs(history, tokens);
+			for (int i = 0; tokens[i] != NULL; i++) {
+				exec_programs(tokens[i]);
 			}
-			execvp(tokens[0], tokens);
+		} else {
+			separate_programs(args, tokens);
+			for (int i = 0; tokens[i] != NULL; i++) {
+				exec_programs(tokens[i]);
+			}
 		}
-		else {
-			wait(NULL);
-		}
+	}
+}
+
+void separate_programs(char *command_line, char *arr_prg[]) {
+	/* Function to separate programs of command line */
+	char *token;
+	int i;
+	
+	token = strtok(command_line, ";\n");
+	for (i = 0; token != NULL; i++) {
+		arr_prg[i] = token;
+		token = strtok(NULL, ";");
+	}
+	arr_prg[i] = NULL; // Indicate the final of args in array
+}
+
+int exec_programs(char *arr_prg) {
+	char *prg[MAX_LENGTH+1];
+	pid_t pid;
+	int i;
+
+	char *token = strtok(arr_prg, " ");
+	for (i = 0; token != NULL; i++) {
+		prg[i] = token;
+		token = strtok(NULL, " ");
+	}
+	prg[i] = NULL;
+
+	pid = fork();
+
+	if (pid < 0) {
+		fprintf(stderr, "Fork failed");
+		return -1;
+	} else if (pid == 0) {
+		execvp(prg[0], prg);
+	} else {
+		wait(NULL);
 	}
 
 	return 0;
-}
-
-void separate_tokens(char *ch, char *arr_ch[]) {
-	/* Function to separate args of typed command line */
-	char *token;
-	int t_i = 0;
-
-	token = strtok(ch, " ");
-	do {
-		arr_ch[t_i] = token;
-		token = strtok(NULL, " ");
-		t_i++;
-	} while (token != NULL);
-	arr_ch[t_i] = NULL; // Indicate the final of args in array
 }
