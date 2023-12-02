111919397a996a7f26e733d637c304b7c1ef94ac
loki0b <riqc0nt4ct@hotmail.com>
2023-12-02 20:58:46 -0300

    refactoring code

--- shell.c
+++ shell.c
@@ -1,51 +1,53 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/types.h>
-#include <sys/wait.h>
-
-#define MAX_LENGTH 80 /* Command line max length */
-#define MAX_TOKENS 40 /* Tokens max length */
+#include "constants.h"
 
 void separate_programs(char *ch, char *arr_ch[]);
 int exec_programs(char *arr_prg);
 
 int main(int argc, char *argv[]) {
 	char args[MAX_LENGTH+1], history[MAX_LENGTH+1], *tokens[MAX_TOKENS+1];
-	int running; /* Flag to determine when to exit program */
-	int run_history; /* Flag to determine when execute last command typed */
 	pid_t pid;
 
-	run_history = 0;
-	running = 1;
+	// Flags
+	int running = TRUE; /* Determine when to exit program */
+	int run_history = FALSE; /* Determine when execute last command typed */
+	int interactive_mode = FALSE;
+	int batch_mode = FALSE;
+	
 	while (running)	{
 		printf("hgl>");
 		fflush(stdout);
 
-		if (argc < 2) { // Interactive mode
+		if (argc < 2) {
+			interactive_mode = TRUE;
+		} else if (argc == 2) {
+			batch_mode = TRUE;
+		} else {
+			return -1;
+		}
+
+		if (interactive_mode) {
 			fgets(args, MAX_LENGTH, stdin);
-		} else if (argc == 2) { // Batch mode
+		} else if (batch_mode) {
 			FILE *fptr;
 			fptr = fopen(argv[1], "r");
 			fgets(args, MAX_LENGTH, fptr);
 			fclose(fptr);
 
-			running = 0;
-		} else {
-			fprintf(stderr, "Invalid arguments");
-			return -1;
+			running = FALSE;
 		}
 
 		args[strcspn(args, "\n")] = 0; // Remove the "\n" char to use exec functions
 
-		if (!(strcmp(args, "exit")))
-			exit(EXIT_SUCCESS);
+		if (!(strcmp(args, "exit"))) // If user type "exit"
+			running = FALSE;
 
 		if (!(strcmp(args, "!!"))) 
-			run_history = 1;
+			run_history = TRUE;
 		else 
-			strcpy(history, args);
+			strcpy(history, args); // Store last command typed
 
 		if (run_history) {
 			separate_programs(history, tokens);
@@ -58,45 +60,9 @@ int main(int argc, char *argv[]) {
 				exec_programs(tokens[i]);
 			}
 		}
+
+		if (!running) {
+			exit(EXIT_SUCCESS);
+		}
 	}
-}
-
-void separate_programs(char *command_line, char *arr_prg[]) {
-	/* Function to separate programs of command line */
-	char *token;
-	int i;
-	
-	token = strtok(command_line, ";\n");
-	for (i = 0; token != NULL; i++) {
-		arr_prg[i] = token;
-		token = strtok(NULL, ";");
-	}
-	arr_prg[i] = NULL; // Indicate the final of args in array
-}
-
-int exec_programs(char *arr_prg) {
-	/* Function to execute the programns in array of args */
-	char *prg[MAX_LENGTH+1];
-	pid_t pid;
-	int i;
-
-	char *token = strtok(arr_prg, " ");
-	for (i = 0; token != NULL; i++) {
-		prg[i] = token;
-		token = strtok(NULL, " ");
-	}
-	prg[i] = NULL;
-
-	pid = fork();
-
-	if (pid < 0) {
-		fprintf(stderr, "Fork failed");
-		return -1;
-	} else if (pid == 0) {
-		execvp(prg[0], prg);
-	} else {
-		wait(NULL);
-	}
-
-	return 0;
 }
