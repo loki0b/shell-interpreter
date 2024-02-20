ff31559aaffaa4cc8f55ed80181eef1c87b50ae1
loki0b <riqc0nt4ct@hotmail.com>
2024-02-20 18:22:40 -0300

    update

--- src/exec_prg.c
+++ src/exec_prg.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-void execute_program(const char *program, const char *args[]) {
+void execute_program(char *program, char *args[]) {
     pid_t pid;
 
-    if (pid = fork() == -1); // TODO: error
+    if (pid = fork() == -1) { /*  TODO: error */ ;}
     else if (pid == 0) {
         // Maybe use a different exec function. TODO: Learn more about exec()
         execvp(program, args);
--- src/main.c
+++ src/main.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "shell_functions.h"
 
 #define BUFFER_SIZE 60
 
-int main() {
+int main(int argc, char *argv[]) {
     bool running = true;
-    char buffer[BUFFER_SIZE];
+    char buffer[BUFFER_SIZE]; 
     
     while (running) {
         printf("sh>");
         fgets(buffer, BUFFER_SIZE, stdin);
+        // execute_program("ls", cmd);
         exit(EXIT_SUCCESS);
     }
 }
