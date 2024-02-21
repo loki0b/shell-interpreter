6f0afd416a71e501f32b4345063645768ac72e08
loki0b <riqc0nt4ct@hotmail.com>
2024-02-21 01:08:07 -0300

    add a question question

--- src/main.c
+++ src/main.c
@@ -13,6 +13,7 @@ int main(int argc, char *argv[]) {
     while (running) {
         printf("sh>");
         fgets(buffer, BUFFER_SIZE, stdin);
+        // Should I encapsulate the separate_cmdline function?
         separate_cmdline(buffer, cmd);
         execute_program(cmd[0], cmd);
         exit(EXIT_SUCCESS);
