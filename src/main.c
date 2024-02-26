1f307ba3daeb66514ae98ece43bf061ea578b927
nouoneq <nouoneq@riseup.net>
2024-02-26 01:13:31 -0300

    Add initial batch setup

--- src/main.c
+++ src/main.c
@@ -6,13 +6,27 @@
 
 int main(int argc, char *argv[]) {
     bool running = true;
-    char *buffer[BUFFER_SIZE];
+    char buffer[BUFFER_SIZE];
+	FILE *batch;	
  
     while (running) {
-        printf("sh>");
-        //TODO: Criar função para input com fgets()
+	// TODO argc sendo 2 para rodar com apenas 1 argumento, futuramente outros casos serão tratados
+	if (argc == 2) {
+		batch = fopen(argv[1], "r");
+		if (batch != NULL) {
+			fgets(buffer, BUFFER_SIZE, batch);
+			//execute_function();
+		} else {
+			fprintf(stderr, "File doesn't exists");
+		}
+			running = false;
+	} else {
+		printf("sh>");
+       	//TODO: Criar função para input com fgets()
         fgets(buffer, BUFFER_SIZE, stdin);
         exit(EXIT_SUCCESS);
-    }
+ 
+		}
+	}
 
 }
